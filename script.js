c6b0eeeba2b56e85e56a7489ba4511993cd5f239
Ekaterina <45600676+EkaterinaGe@users.noreply.github.com>
2021-11-19 13:09:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -37,21 +37,21 @@ let choiceSite = 0
 
 const orderSite = function () {
     choiceSite = choiceSite + 1
-    console.log(choiceSite);
+    console.log("Выбрали сделать сайт " + choiceSite " раз");
 }
 
 let choiceMobile = 0
 
 const orderMobile = function () {
     choiceMobile = choiceMobile + 1
-    console.log(choiceMobile);
+    console.log("Выбрали сделать приложение " + choiceMobile + " раз");
 }
 
 let ch0iceAudit = 0
 
 const orderAudit = function () {
     ch0iceAudit = ch0iceAudit + 1
-    console.log(ch0iceAudit);
+    console.log("Выбрали провести аудит " + ch0iceAudit + " раз");
 }
 
 let registration = document.getElementById("my-modal-registration");
@@ -64,7 +64,7 @@ let clickRegistration = 0
 registrations.onclick = function () {
     registration.style.display = "block";
     clickRegistration = clickRegistration + 1
-    console.log(clickRegistration);
+    console.log("Нажали на кнопку регистрации " + clickRegistration + " раз");
 }
 
 let closeRegistration = 0
@@ -72,7 +72,7 @@ let closeRegistration = 0
 closes.onclick = function () {
     registration.style.display = "none";
     closeRegistration = closeRegistration + 1
-    console.log(closeRegistration);
+    console.log("Закрыли окно регистрации " + closeRegistration + " раз");
 }
 
 let clickLogup = 0
@@ -80,7 +80,7 @@ let clickLogup = 0
 logup.onclick = function () {
     registration.style.display = "none";
     clickLogup = clickLogup + 1
-    console.log(clickLogup);
+    console.log("Зарегистрировались " + clickLogup + " раз");
 }
 
 window.onclick = function (event) {
@@ -94,7 +94,7 @@ let names = document.getElementById("names")
 let clickName = 0
 names.onfocus = function () {
     clickName = clickName + 1
-    console.log(clickName);
+    console.log("Ввели имя для регистрации " + clickName + " раз");
 }
 
 let input = document.getElementById("email")
@@ -115,7 +115,7 @@ input.onfocus = function() {
         error.innerHTML = "";
     }
     clickEmail = clickEmail + 1
-    console.log(clickEmail);
+    console.log("Ввели email для регистрации " + clickEmail + " раз");
 };
 
 let password = document.getElementById("password")
@@ -123,7 +123,7 @@ let password = document.getElementById("password")
 let clickPassword = 0
 password.onfocus = function() {
     clickPassword = clickPassword + 1
-    console.log(clickPassword);
+    console.log("Ввели пароль для регистрации " + clickPassword + " раз");
 }
 
 let login = document.getElementById("my-modal-login");
@@ -136,7 +136,7 @@ let clickEnter = 0
 logins.onclick = function () {
     login.style.display = "block";
     clickEnter = clickEnter + 1
-    console.log(clickEnter);
+    console.log("Нажали на кнопку входа " + clickEnter + " раз");
 }
 
 let closeLogin = 0
@@ -144,7 +144,7 @@ let closeLogin = 0
 shut.onclick = function () {
     login.style.display = "none";
     closeLogin = closeLogin + 1
-    console.log(closeLogin);
+    console.log("Закрыли окно входа " + closeLogin + " раз");
 }
 
 let clickLogin = 0
@@ -152,7 +152,7 @@ let clickLogin = 0
 enter.onclick = function () {
     login.style.display = "none";
     clickLogin = clickLogin + 1
-    console.log(clickLogin);
+    console.log("Вошли на сайт " + clickLogin + " раз");
 }
 
 window.onclick = function (event) {
@@ -166,7 +166,7 @@ let nick = document.getElementById("nick")
 let clickNick = 0
 nick.onfocus = function () {
     clickNick = clickNick + 1
-    console.log(clickNick);
+    console.log("Ввели имя для входа " + clickNick + " раз");
 }
 
 let parole = document.getElementById("parole")
@@ -174,7 +174,7 @@ let parole = document.getElementById("parole")
 let clickParole = 0
 parole.onfocus = function() {
     clickParole = clickParole + 1
-    console.log(clickParole);
+    console.log("Ввели пароль для входа " + clickParole + " раз");
 }
 
 
@@ -188,19 +188,19 @@ let clickSite = 0
 sites.onclick = function () {
     site.style.display = "block";
     clickSite = clickSite + 1
-    console.log(clickSite);
+    console.log("Нажали на кнопку заказа сайта " + clickSite + " раз");
 }
 
 cover.onclick = function () {
     site.style.display = "none";
     closeOrder = closeOrder + 1
-    console.log(closeOrder);
+    console.log("Закрыли окно сделать заказ " + closeOrder + " раз");
 }
 
 book.onclick = function () {
     site.style.display = "none";
     doOrder = doOrder + 1
-    console.log(doOrder);
+    console.log("Сделали заказ " + doOrder + " раз");
 }
 
 window.onclick = function (event) {
@@ -219,19 +219,19 @@ let clickMobile = 0
 mobiles.onclick = function () {
     mobile.style.display = "block";
     clickMobile = clickMobile + 1
-    console.log(clickMobile);
+    console.log("Нажали на кнопку заказать мобильное приложение " + clickMobile + " раз");
 }
 
 turnoff.onclick = function () {
     mobile.style.display = "none";
     closeOrder = closeOrder + 1
-    console.log(closeOrder);
+    console.log("Закрыли окно сделать заказ " + closeOrder + " раз");
 }
 
 buy.onclick = function () {
     mobile.style.display = "none";
     doOrder = doOrder + 1
-    console.log(doOrder);
+    console.log("Сделали заказ " + doOrder + " раз");
 }
 
 window.onclick = function (event) {
@@ -250,19 +250,19 @@ let clickAudit = 0
 audits.onclick = function () {
     audit.style.display = "block";
     clickAudit = clickAudit + 1
-    console.log(clickAudit);
+    console.log("Нажали на кнопку заказать аудит " + clickAudit + " раз");
 }
 
 adjourn.onclick = function () {
     audit.style.display = "none";
     closeOrder = closeOrder + 1
-    console.log(closeOrder);
+    console.log("Закрыли окно сделать заказ " + closeOrder + " раз");
 }
 
 purchase.onclick = function () {
     audit.style.display = "none";
     doOrder = doOrder + 1
-    console.log(doOrder);
+    console.log("Сделали заказ " + doOrder + " раз");
 }
 
 window.onclick = function (event) {
@@ -284,72 +284,78 @@ function nextSlide() {
 let goUmbrella = 0
 const clickUmbrella = function () {
     goUmbrella = goUmbrella + 1
-    console.log(goUmbrella);
+    console.log("Нажали на кнопку перехода на сайт UmbrellaIT " + goUmbrella + " раз");
 }
 
 let goYandex = 0
 const clickYandex = function () {
     goYandex = goYandex + 1
-    console.log(goYandex);
+    console.log("Нажали на кнопку перехода на сайт Яндекс " + goYandex + " раз");
 }
 
 let goGoogle = 0
 const clickGoogle = function () {
     goGoogle = goGoogle + 1
-    console.log(goGoogle);
+    console.log("Нажали на кнопку перехода на сайт Гугл " + goGoogle + " раз");
 }
 
 let goLanit = 0
 const clickLanit = function () {
     goLanit = goLanit + 1
-    console.log(goLanit);
+    console.log("Нажали на кнопку перехода на сайт ЛАНИТ " + goLanit + " раз");
 }
 
 let goOzon = 0
 const clickOzon = function() {
     goOzon = goOzon + 1
-    console.log(goOzon);
+    console.log("Нажали на кнопку перехода на сайт Озон " + goOzon + " раз");
 }
 
 let goSoftline = 0 
 const clickSoftline = function () {
     goSoftline = goSoftline + 1
-    console.log(goSoftline);
+    console.log("Нажали на кнопку перехода на сайт Softline " + goSoftline + " раз");
 }
 
 let goMarvel = 0
 const clickMarvel = function () {
     goMarvel = goMarvel + 1
-    console.log(goMarvel);
+    console.log("Нажали на кнопку перехода на сайт Марвел-Дистрибуция " + goMarvel + " раз");
 }
 
 let goLamoda = 0 
 const clickLamoda = function () {
     goLamoda = goLamoda + 1
-    console.log(goLamoda);
+    console.log("Нажали на кнопку перехода на сайт Lamoda Tech " + goLamoda + " раз");
 }
 
 let goDistribution = 0
 const clickDistribution = function () {
     goDistribution = goDistribution + 1
-    console.log(goDistribution);
+    console.log("Нажали на кнопку перехода на сайт OCS Distribution " + goDistribution + " раз");
 }
 
 let goRostec = 0
 const clickRostec = function () {
     goRostec = goRostec + 1
-    console.log(goRostec);
+    console.log("Нажали на кнопку перехода на сайт Ростех " + goRostec + " раз");
 }
 
 let pictureIndex = 1;
 showPicture(pictureIndex);
 
+let nextButton = 0
 function nextPicture() {
     showPicture(pictureIndex += 1);
+    nextButton = nextButton + 1
+    console.log("Нажали на кнопку перехода на следующий слайд " + nextButton + " раз")
 }
 
+let previousButton = 0              
 function previousPicture() {
     showPicture(pictureIndex -= 1);  
+    previousButton = previousButton + 1
+    console.log("Нажали на кнопку перехода на предидущий слайд " + previousButton + " раз")
 }
 
 function currentPicture(n) {
